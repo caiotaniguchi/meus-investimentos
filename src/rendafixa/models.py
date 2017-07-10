204f922d2664fb10918ba5e4c2fef57143a0ef58
Caio Souza <caiosouza@CSRJL152M.local>
2017-07-10 20:18:28 -0300

    Adiciona modelo e view para renda fixa

--- src/rendafixa/models.py
+++ src/rendafixa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class RendaFixa(models.Model):
+    def __init__(self, data_inicio, data_fim, valor_aporte):
+        self.data_inicio = data_inicio
+        self.data_fim = data_fim
+        self.valor_aporte = valor_aporte
+
+    def calcular_valor_atual(self):
+        pass
